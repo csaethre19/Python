339279c0a9167b61326cd15e68bbcd8318843a85
csaethre19 <csaethre@bruinmail.slcc.edu>
2020-08-13 12:15:32 -0600

    changes made to main

--- conditionals.py
+++ conditionals.py
@@ -1,9 +1,12 @@
 def main():
     x, y = 10, 100
+    if (x < y):
+        st = "x is less than y"
+    else:
+        st = "x is greater than y"
 
-if (x < y):
-    st = "x is less than y"
-else:
-    st = "x is greater than y"
+    print(st)
+    
+if __name__ == '__main__':
+    main()
 
-print(st)
